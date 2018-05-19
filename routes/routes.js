c0b2e435bf2011a8b5dbc5124c1fccdf4a5bb7ff
Brian Strylowski <brianstry@hotmail.com>
2018-05-19 12:33:34 -0400

    welp

--- routes/routes.js
+++ routes/routes.js
@@ -1,193 +1,204 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   //Home Page
   //=======================================================
-  app.get("/", function(req, res) {
-        db.Park.findAll({}).then(function(dbPark) {
-       
-        var hbsObject = {
-          parks: dbPark,
-          home: {selected : true}
-        };
-         res.render("index", hbsObject);
-               
-        
-      });
-  }); 
+  app.get("/", function (req, res) {
+    db.Park.findAll({}).then(function (dbPark) {
+
+      var hbsObject = {
+        parks: dbPark,
+        home: { selected: true }
+      };
+      res.render("index", hbsObject);
+
+
+    });
+  });
 
   //Parks A-Z 
   //====================================================== 
-  app.get("/all-parks", function(req, res) {
-      db.Park.findAll({        
-        order:  ["name"]
-          
-      }).then(function(dbPark) {
+  app.get("/all-parks", function (req, res) {
+    db.Park.findAll({
+      order: ["name"]
+
+    }).then(function (dbPark) {
       var hbsObject = {
         parks: dbPark,
-        az : {selected : true}
+        az: { selected: true }
       };
-         
+
       res.render("all-parks", hbsObject);
-      });
+    });
   });
 
   //All Features
   //=====================================================
-  app.get("/all-features", function(req, res) {
-    db.Park.findAll({}).then(function(dbPark) {
-    var hbsObject = {
-      parks: dbPark,
-      feat : {selected : true}
-    };
-        
-    res.render("all-features", hbsObject);
+  app.get("/all-features", function (req, res) {
+    db.Park.findAll({}).then(function (dbPark) {
+      var hbsObject = {
+        parks: dbPark,
+        feat: { selected: true }
+      };
+
+      res.render("all-features", hbsObject);
     });
   });
 
   //About
   //=====================================================
-  app.get("/about", function(req, res) {
+  app.get("/about", function (req, res) {
     db.Park.findAll({
-      order:  ["name"]
-    }).then(function(dbPark) {
-    var hbsObject = {
-      parks: dbPark,
-      about : {selected : true}
-    };              
+      order: ["name"]
+    }).then(function (dbPark) {
+      var hbsObject = {
+        parks: dbPark,
+        about: { selected: true }
+      };
       res.render("about", hbsObject);
     });
   });
 
   //Add Park
   //===================================================== 
-  app.get("/add-park", function(req, res) {
-      db.Park.findAll({}).then(function(dbPark) {
+  app.get("/add-park", function (req, res) {
+    db.Park.findAll({}).then(function (dbPark) {
       var hbsObject = {
         parks: dbPark,
-        add : {selected : true}
+        add: { selected: true }
       };
-              
+
       res.render("add-park", hbsObject);
-      });
+    });
   });
 
-  app.get("/add-feature", function(req, res) {
-    db.Park.findAll({        
-      order:  ["name"]
-        
-    }).then(function(dbPark) {
-    var hbsObject = {
-      parks: dbPark,
-      az : {selected : true}
-    };
-            
-    res.render("add-feature", hbsObject);
+  app.get("/add-feature", function (req, res) {
+    db.Park.findAll({
+      order: ["name"]
+
+    }).then(function (dbPark) {
+      var hbsObject = {
+        parks: dbPark,
+        az: { selected: true }
+      };
+
+      res.render("add-feature", hbsObject);
     });
-});
+
+  });
 
   //Individual Feature Page
   //=====================================================
-  app.get("/features/:feature", function(req, res) {
+  app.get("/features/:feature", function (req, res) {
     var feature = req.params.feature;
     var whereClause = {};
     console.log(feature);
-    switch(feature) {
-      case "basketball":          
-      whereClause = {basketball : true}; 
-      break;
+    switch (feature) {
+      case "basketball":
+        whereClause = { basketball: true };
+        break;
 
       case "dogPark":
-      whereClause = {dogPark : true};    
-      break;
+        whereClause = { dogPark: true };
+        break;
 
       case "golf":
-      whereClause = {golf : true};    
-      break;
+        whereClause = { golf: true };
+        break;
 
       case "handiAcces":
-      whereClause = {handiAcces : true};    
-      break;
+        whereClause = { handiAcces: true };
+        break;
 
       case "playground":
-      whereClause = {playground : true};    
-      break;
+        whereClause = { playground: true };
+        break;
 
       case "soccer":
-      whereClause = {soccer : true};    
-      break;
+        whereClause = { soccer: true };
+        break;
 
       case "tennis":
-      whereClause = {tennis : true};    
-      break;
+        whereClause = { tennis: true };
+        break;
 
       case "trails":
-      whereClause = {trails : true};    
-      break;
+        whereClause = { trails: true };
+        break;
 
       case "vendingMachines":
-      whereClause = {vendingMachines : true};    
-      break;
+        whereClause = { vendingMachines: true };
+        break;
 
       case "views":
-      whereClause = {views : true};    
-      break;
+        whereClause = { views: true };
+        break;
 
       case "waterSports":
-      whereClause = {waterSports : true};    
-      break;
+        whereClause = { waterSports: true };
+        break;
 
       case "workoutGear":
-      whereClause = {workoutGear : true};    
-      break;
+        whereClause = { workoutGear: true };
+        break;
 
       case name:
-      whereClause = {name : req.param.body}
-        
-     
+        whereClause = { name: req.param.body }
+
+
     }
-    db.Park.findAll({  
-      where: whereClause,  
+    db.Park.findAll({
+      where: whereClause,
       order: ["name"]
-    }).then(function(dbPark) {     
+    }).then(function (dbPark) {
       var hbsObject = {
         parks: dbPark,
         layout: "main"
       };
-     
+
       res.render("feature", hbsObject);
-              
-      });
+
+    });
   });
 
   //Individual Park Page 
   //=====================================================
 
-  app.get("/:name", function(req, res) {
-      db.Park.findOne({
-        where: {
-          name : req.params.name
-        }        
-      }).then(function(dbPark) {     
+  app.get("/:name", function (req, res) {
+    db.Park.findOne({
+      where: {
+        name: req.params.name
+      }
+    }).then(function (dbPark) {
       var hbsObject = {
         parks: dbPark
       };
-  
-              
+
+
       res.render("park", hbsObject);
     });
   });
- 
+
 
   // API Post
   //======================================================= 
 
-  app.post("/api/parks", function(req, res) {
-      db.Park.create(req.body).then(function(dbPark) {
-      res.json(dbPark);    
+  app.post("/api/parks", function (req, res) {
+    db.Park.create(req.body).then(function (dbPark) {
+      res.json(dbPark);
     });
-  }); 
+  });
 
-
-
+  app.post("/api/features", function (req, res) {
+    var query = {};
+    if (req.query.parks.id) {
+      query.ParksId = req.query.parks.id;
+    }
+    db.Feature.findAll({
+      where: query,
+      include: [db.Parks]
+    }).then(function (dbPark) {
+      res.json(dbPark);
+    });
+  });
 };
