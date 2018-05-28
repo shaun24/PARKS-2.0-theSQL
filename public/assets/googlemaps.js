72362c9b0a325d2140b0036967af9a3557c00134
John D <johndekis@gmail.com>
2018-05-28 18:26:59 -0400

    marker functions for basketball, restrooms and wheelchair access

--- public/assets/googlemaps.js
+++ public/assets/googlemaps.js
@@ -5,59 +5,100 @@ function initMap() {
         zoom: 9
     });
 
-    //function to pull in data
+    //function to pull in data and make pins for ALL parks in db
     getParks();
-    //makeMarkers(parkArray);
+    
+    
     
     // marker making functions
     //=======================================================
-    
-    //array of markers TO BE REPLACED WITH DB DATA
-    const bballParks = [{
-        coords:  [35.1945, -80.8420],
-        content: `<h6><a href="/Freedom">Freedom Park</a></h6>`,
-        }];
+    var markers = [] // array to hold google marker objects
+    var parkArray =[]; //array to hold objects for markers with info boxes and links
 
-    const markers = [
-        {
-         coords:  [35.227,-80.8373],
-         content: `<h6><a href="/First Ward">First Ward Park</a></h6>`,
-        },
-        {
-        coords:  [35.2271, -80.8431],
-        content: `<h6>Charlotte, North Carolina</h6>`,
-        },
-        {
-        coords:  [35.1505, -80.7415],
-        content: `<h6>McAlpine Creek Park</h6>`,
-        },
-        {
-        coords:  [35.1945, -80.8420],
-        content: `<h6>Freedom Park</h6>`,
-        }
-        
-    ];
-   
-    var basketballBox = document.getElementById("basketball");
-    basketballBox.addEventListener('click', function(){
-        
-        makeMarkers(bballParks);
-        console.log(`console log`)
+    const bballParks = [],
+        wheelChairParks = [],
+        restroomParks = [];
+
+    // Handicap Access and Restrooms
+    //=====================================
+    // Handicap Access Markers
+    var wheelChairBox = document.getElementById("handiAccess");
+    wheelChairBox.isChecked = false;
+    wheelChairBox.addEventListener('click', function(){
+        if(!wheelChairBox.isChecked){
+            wheelChairBox.isChecked = true;
+            removeMarkers(markers);
+            makeMarkers(wheelChairParks);
+        } else {
+            removeMarkers(markers);
+            wheelChairBox.isChecked = false;
+        }        
+        //console.log(parkArray);
+        console.log(wheelChairBox.isChecked);
+        //removeMarkers(parkArray);        
+        console.log(`Parks with wheelChair ramps`);
     });
 
+
+    // Restroom Markers
+    var restroomBox = document.getElementById("restrooms");
+    restroomBox.isChecked = false;
+    restroomBox.addEventListener('click', function(){
+        if(!restroomBox.isChecked){
+            restroomBox.isChecked = true;
+            removeMarkers(markers);
+            makeMarkers(restroomParks);
+        } else {
+            removeMarkers(markers);
+            restroomBox.isChecked = false;
+        }        
+        //console.log(parkArray);
+        console.log(restroomBox.isChecked);
+        //removeMarkers(parkArray);        
+        console.log(`Parks with restrooms`);
+    });
+
+
+    // Basketball Markers
+    var basketballBox = document.getElementById("basketball");
+    basketballBox.isChecked = false;
+    basketballBox.addEventListener('click', function(){
+        if(!basketballBox.isChecked){
+            basketballBox.isChecked = true;
+            removeMarkers(markers);
+            makeMarkers(bballParks);
+        } else {
+            removeMarkers(markers);
+           // marker2.setMap(null);
+            basketballBox.isChecked = false;
+        }        
+        //console.log(parkArray);
+        console.log(basketballBox.isChecked);
+        //removeMarkers(parkArray);        
+        console.log(`Parks with Basketball Courts`);
+    });
+
+
     //call markerMaker Array
-    //makeMarkers(markers);
 
     function makeMarkers(array){
         for(let i = 0; i < array.length; i++){
             //add logic here to filter array based on features
             addMarker(array[i]);
+
         }
+    }
+
+    function removeMarkers(array){
+        for(let i = 0; i < array.length; i++){
+            array[i].setMap(null);
+        }
+        console.log(`Removed!`);
     }
 
     function addMarker(props) {
         let infoWindow3;
-        let marker2 = new google.maps.Marker({
+         let marker2 = new google.maps.Marker({
             position: new google.maps.LatLng(props.coords[0], props.coords[1]),
             animation: google.maps.Animation.DROP,
             map: map
@@ -84,106 +125,85 @@ function initMap() {
             map.setZoom(11);
             map.setCenter({ lat: 35.2271, lng: -80.8431 });
         })
+
+        markers.push(marker2);
     }
 
     
-function getParks(){
-    $.ajax({
-        method: "GET",
-        url: '/api/parks'
-    }).then(function(result){ 
-        console.log(result);       
-        var parkArray = [];
-        for(let i=0; i < result.length; i++){
-            let parkName = result[i].name;
-            //this should take out the spaces ==> just realized the links use the space and capitalization, here if needed
-            //let link = parkName.replace(/ /g, '').toLowercase;
-            //console.log(link);
+    function getParks(){
+        $.ajax({
+            method: "GET",
+            url: '/api/parks'
+        }).then(function(result){ 
 
-            parkArray.push({
-                coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
-                content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
-            });
+            console.log(result);
+                        
+            //loop throught result
+            for(let i=0; i < result.length; i++){
+                let parkName = result[i].name;
+                let features = result[i].Features;
+
+                if(result[i].handiAccess){
+                    wheelChairParks.push({
+                        coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
+                        content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
+                    });
+                }
+                
+                if(result[i].restrooms){
+                    restroomParks.push({
+                        coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
+                        content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
+                    });
+                }
+
+
+                // check for features
+                if(features.length > 0){
+                    //console.log(`we got features`);
+                    for(let j=0; j < features.length; j++){
+
+                        // check for parks with basketball courts
+                        if(features[j].name === "Basketball"){
+                            bballParks.push({
+                                coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
+                                content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
+                            });
+                        }
+                    
+                        // check for parks with basketball courts
+                        if(features[j].name === "Basketball"){
+                            bballParks.push({
+                                coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
+                                content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
+                            });
+                        }
+                    
+                    }
+
+
+                //console.log(result[i].Features);
+
+                //this should take out the spaces ==> just realized the links use the space and capitalization, here if needed
+                //let link = parkName.replace(/ /g, '').toLowercase;
+                //console.log(link);
+              
+            }
+            //console.log(parkArray);
+        var marker = {
+                    coords: [parseFloat(result[i].lat), parseFloat(result[i].lng)],
+                    content: `<h5><a href="/${parkName}"> ${parkName} Park</a></h5>`
+                };
+                parkArray.push(marker);
+
+
+            //return parkArray
+            makeMarkers(parkArray)
+            console.log(markers);
         }
-        console.log(parkArray);
-        //return parkArray
-        makeMarkers(parkArray)
     });
-}
+    
 
+} 
 
-} // end of init map
-
-// This function pulls json from  the db and parses it into coords and content for markers and windows
-
-            
-// Geocoding Funcitons: example makes a list 
-//=========================================================
-//Call Geocode
-//geocode();
-
-//get location  form
-//var locationForm = document.getElementById('pin-form');
-
-//locationForm.addEventListener('submit', geocode)
-
-//not in use currently
-function geocode(event, address){
-    //change this to checkbox value to find address based on activity selected
-    let address = document.getElementById('input-id').value;
-    event.pereventDefault();
-
-    //var loaction = address;
-    axios.get('https:maps.googleapis.com/api/geocode/json', {
-        params: {
-            address: address,
-            key: 'API KEY'
-        }
-    })
-    .then(function(response){
-        //log full response
-        console.log(response);
-
-        //Formatted Address
-        var formattedAddress = response.data.results[0].formatted_address;
-        var fromattedAddressOutput = `
-        <ul>
-            <li> ${formattedAddress}</li>
-        </ul>
-        `;
-       
-
-
-        //Address Components
-        var addressComponents = response.data.results[0].address_componets;
-        var addressComponentsOutput = ` <ul> `;
-        for(let i = 0; i < addressComponents.length; i++){
-            addressComponentsOutput += `
-            <li><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>
-            `;
-        }
-
-        addressComponentsOutput += `</ul>`;
-
-        //Geometry
-        var lat = response.data.results[0].geometry.location.lat;
-        var lng = response.data.results[0].geometry.location.lng;
-
-        var geometryOutput = `
-        <ul>
-            <li> <strong>Latitude</strong>${lat}</li>
-            <li> <strong>Longitude</strong>${lng}</li>
-        </ul>
-        `;
-
-
-        //output to app
-        document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
-        document.getElementById('address-components').innerHTML = addressComponentsOutput;
-        document.getElementById('geometry').innerHTML = geometryOutput;
-    })
-    .catch(function(error){
-        console.log(error);
-    });
-}
-
+}// end of init map
