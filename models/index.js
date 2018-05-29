86c44059745ccf82c458895c2206de34b5c789b4
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-05-29 19:52:48 -0400

    modified index.js to try and fix deploy error

--- models/index.js
+++ models/index.js
@@ -10,7 +10,8 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL, config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  // var sequelize = new Sequelize(process.env.JAWSDB_URL, config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
